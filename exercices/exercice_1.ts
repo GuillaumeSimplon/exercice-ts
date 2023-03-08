91d40e464b329ab5ff6ae19d3cdb7192e64ae481
Guillaume <estrade.guillaume.simplon@gmail.com>
2023-03-08 14:59:33 +0100

    exercice ts

---
+++ exercices/exercice_1.ts
@@ -0,0 +1,46 @@
+// Exercice # 1
+
+// - Écrivez la suite d'actions à réaliser pour vérifier si le ticket de loterie que le client vous tend est gagnant.
+// - Si le chiffre est compris entre 1 et 5 annoncer au client qu'il a gagné, autrement indiquer lui qu'il à perdu et invitez le à rejouer.
+// - Écrivez cette algortihme en pseudo code à l'aide des commentaires Javascript.
+
+
+// Le client remet le ticket
+// La machine lit le ticket
+
+// Si le chiffre est compris entre 1 et 5
+// Annoncer que le client a gagné
+
+// Si le chiffre n'est pas compris entre 1 et 5
+// Annoncer que le client à perdu
+// Dans ce cas, l'inviter à retenter sa chance
+
+
+
+
+// Exerice # 2
+
+// - Écrivez la suite d'actions à réaliser pour connaitre le montant des courses du clients TTC.
+// - Votre caisse enregistreuse est cassées, vous devez aditionner les prix HT de chaque produit du client et y ajouter la TVA de 20%.
+// - Écrivez cette algortihme en pseudo code à l'aide des commentaires Javascript.
+
+
+// Le client dépose son panier sur le tapis
+// Je note le prix de chaque article
+// j'additionne le total des prix des articles pour un résultat HT
+// J'ajouter 20% au tatal HT pour obtenir le résultat TTC
+
+
+
+
+// Exercice # 3
+
+// - Typer les constantes suivantes :
+// const organism: string = "Simplon"
+// const learners: number = 15
+// const promotionStarted: boolean = true
+// const learnerNames: string[] = ["Vincent", "Toufik", "Tom", "Samuel", "Orhan"]
+// const pedagogicalReferent: array = { name: "Tiphaine", work: "Simplon" }
+// const absentLearner: undefined = undefined
+// const excludedLearner: any = unknow
+
